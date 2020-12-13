377aed136e896a21e72934a9fd4775337ab821e0
Neeraj Kashyap <nkashy1@gmail.com>
2020-12-13 11:57:55 -0800

    Renamed CLI bug -> bugout
    Added `--version` flag to bugout and also added shell competions

---
+++ cmd/bugout/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/bugout-dev/bugout-go/cmd/bugout/brood"
+	bugout "github.com/bugout-dev/bugout-go/pkg"
+)
+
+func CreateBugoutCommand() *cobra.Command {
+	bugoutCmd := &cobra.Command{
+		Use:   "bugout",
+		Short: "Interact with Bugout from your command line",
+		Long: `Bugout: The knowledge base for software teams
+
+The bugout utility lets you interact with your Bugout resources from your command line.`,
+		Version: bugout.Version,
+	}
+
+	broodCmd := brood.CreateBroodCommand()
+	completionCmd := CreateBugoutCompletionCommand()
+
+	bugoutCmd.AddCommand(broodCmd, completionCmd)
+
+	return bugoutCmd
+}
+
+func CreateBugoutCompletionCommand() *cobra.Command {
+	completionCmd := &cobra.Command{
+		Use:   "completion",
+		Short: "Generate shell completion scripts for the bugout tool",
+		Long: `Generate shell completion scripts for the bugout tool
+
+You can source these generated scripts or add them in the appropriate completion directories to
+unlock bugout command completion for your shell.
+
+For example, to activate bash completions:
+	$ . <(bugout completion bash)`,
+	}
+
+	bashCompletionCmd := &cobra.Command{
+		Use:   "bash",
+		Short: "bash completions for bugout",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Root().GenBashCompletion(cmd.OutOrStdout())
+		},
+	}
+
+	zshCompletionCmd := &cobra.Command{
+		Use:   "zsh",
+		Short: "zsh completions for bugout",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Root().GenZshCompletion(cmd.OutOrStdout())
+		},
+	}
+
+	fishCompletionCmd := &cobra.Command{
+		Use:   "fish",
+		Short: "fish completions for bugout",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Root().GenFishCompletion(cmd.OutOrStdout(), true)
+		},
+	}
+
+	powershellCompletionCmd := &cobra.Command{
+		Use:   "powershell",
+		Short: "powershell completions for bugout",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Root().GenPowerShellCompletion(cmd.OutOrStdout())
+		},
+	}
+
+	completionCmd.AddCommand(bashCompletionCmd, zshCompletionCmd, fishCompletionCmd, powershellCompletionCmd)
+
+	return completionCmd
+}
+
+func main() {
+	bugCmd := CreateBugoutCommand()
+	err := bugCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ pkg/version.go
@@ -0,0 +1,3 @@
+package bugout
+
+const Version string = "0.1.0"
